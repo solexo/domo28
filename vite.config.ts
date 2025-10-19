d6e2e7dbb8783465409e97ced00e00c71ed365e3
User <user@example.com>
2025-10-19 17:28:29 +0100

    Fix base path for dev and production

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Domo28/',
+  base: process.env.NODE_ENV === 'production' ? '/Domo28/' : '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
